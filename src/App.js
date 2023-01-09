6d9aac5f50b8538cb3fa72ff762d0055d9eded73
Lenin K <lenin.kasinathan@zylensolutions.com>
2023-01-10 02:17:41 +0530

    Layout changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,78 @@
-
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import Routing from "./routers/Routing";
+// Import scss
+import "./assets/scss/theme.scss";
+// import Routing from "./routers/Routing";
 import { createBrowserHistory } from "history";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+
+import { Switch, BrowserRouter } from "react-router-dom";
+import { connect } from "react-redux";
+
+// Import Routes all
+import { authProtectedRoutes, publicRoutes } from "./routers";
+
+// Import all middleware
+import Authmiddleware from "./routers/route";
+
+// layouts Format
+import VerticalLayout from "./components/VerticalLayout/";
+import HorizontalLayout from "./components/HorizontalLayout/";
+import NonAuthLayout from "./components/NonAuthLayout";
 
 const defaultHistory = createBrowserHistory();
 
-function App() {
+function App(props) {
+  function getLayout() {
+    let layoutCls = VerticalLayout;
+    switch (props.layout.layoutType) {
+      case "horizontal":
+        layoutCls = HorizontalLayout;
+        break;
+      default:
+        layoutCls = VerticalLayout;
+        break;
+    }
+    return layoutCls;
+  }
+
+  const Layout = getLayout();
+
   return (
     <div className="App">
       <BrowserRouter history={defaultHistory}>
-        <Routing />
-        <ToastContainer />
+        {/* <Routing /> */}
+        <Switch>
+          {publicRoutes.map((route, idx) => (
+            <Authmiddleware
+              path={route.path}
+              layout={NonAuthLayout}
+              component={route.component}
+              key={idx}
+              isAuthProtected={false}
+              exact
+            />
+          ))}
+
+          {authProtectedRoutes.map((route, idx) => (
+            <Authmiddleware
+              path={route.path}
+              layout={Layout}
+              component={route.component}
+              key={idx}
+              isAuthProtected={true}
+              exact
+            />
+          ))}
+        </Switch>
       </BrowserRouter>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    layout: state.Layout,
+  };
+};
+
+export default connect(mapStateToProps, null)(App);
+
--- src/routers/Routing.js
+++ src/routers/Routing.js
@@ -3,12 +3,16 @@ import { Routes, Route } from "react-router-dom";
 // import ErrorPage from "../globalComponent/ErrorPage/Error";
 import AppointmentFrontend from "../pages/AppointmentsFrontend/AppointmentFrontend";
 import PatientFrontend from "../pages/PatientsFrontend/PatientFrontend";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Authentication/Login";
+
 const Routing = () => {
   return (
     <>
       <Routes>
         {/* Public routes */}
-        {/* <Route path="/" element={<Login />} /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/appointment" element={<AppointmentFrontend />} />
         <Route path="/patient" element={<PatientFrontend />} />
 
