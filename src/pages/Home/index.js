1098e3746e2be07860baf42f609ad4dd10f5fe53
Lenin K <lenin.kasinathan@zylensolutions.com>
2023-01-12 05:12:11 +0530

    Login changes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import VerticalLayout from "../../components/VerticalLayout";
+import AppointmentFrontend from "../AppointmentsFrontend/AppointmentFrontend";
+import PatientFrontend from "../PatientsFrontend/PatientFrontend";
 const Home = () => {
   return (
     <VerticalLayout>
       <div className="page-content">
         <h1>Home</h1>
+        <AppointmentFrontend />
+        <PatientFrontend />
       </div>
     </VerticalLayout>
   );
--- src/routers/Routing.js
+++ src/routers/Routing.js
@@ -4,12 +4,13 @@ import { Routes, Route } from "react-router-dom";
 import AppointmentFrontend from "../pages/AppointmentsFrontend/AppointmentFrontend";
 import PatientFrontend from "../pages/PatientsFrontend/PatientFrontend";
 import Home from "../pages/Home";
+import Login from "../pages/Authentication/Login";
 const Routing = () => {
   return (
     <>
       <Routes>
         {/* Public routes */}
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
         <Route path="/appointment" element={<AppointmentFrontend />} />
         <Route path="/appointment/create" element={<AppointmentFrontend />} />
         <Route path="/appointment/edit" element={<AppointmentFrontend />} />
